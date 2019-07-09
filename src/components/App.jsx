8b8b624ce33ffb5fc78bbb0eadbe062206527c70
Berk Atik <s.berkatik@gmail.com>
2019-07-09 11:21:12 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,10 @@ export default class App extends Component {
 
     render() {
         return (
+            <div>
+                test!!!!
             <Kanban data={initialData}></Kanban>
+            </div>
         )
     }
 }
