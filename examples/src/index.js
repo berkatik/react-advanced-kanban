b8a0b1b2f7245bd22da70f350a24be08c753d8d4
Berk Atik <s.berkatik@gmail.com>
2019-07-18 20:49:34 +0300

    Updates

--- examples/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Setup from './Setup';
-
-const App = () => (
-    <Setup />
-);
-render(<App />, document.getElementById("root"));
